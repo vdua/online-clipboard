b4a94c6d908e3c8d2b96063cdaa8bfa0e5384641
vdua <varundua16@gmail.com>
2014-09-15 16:38:47 +0530

    adding ACL on /content/fw to deny anonymous access

--- server/core/src/main/java/com/fw/oc/service/impl/OnlineClipboardServiceImpl.java
+++ server/core/src/main/java/com/fw/oc/service/impl/OnlineClipboardServiceImpl.java
@@ -6,12 +6,22 @@ import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.api.resource.*;
+import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.commons.json.JSONObject;
 import org.apache.sling.jcr.api.SlingRepository;
 import org.apache.sling.commons.json.JSONArray;
+
 import javax.jcr.*;
 
 import com.day.cq.commons.jcr.JcrConstants;
+import org.apache.sling.jcr.base.util.AccessControlUtil;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
@@ -22,21 +32,61 @@ import java.util.ArrayList;
  * Time: 1:17 PM
  */
 @Component(label = "Online Clipboard Data Paste Service",
-           description = "Service for Pasting Data on Online Clipboard",
-           immediate = true,
-           metatype = true)
+        description = "Service for Pasting Data on Online Clipboard",
+        immediate = true,
+        metatype = true)
 @Service(OnlineClipboardService.class)
 public class OnlineClipboardServiceImpl implements OnlineClipboardService {
     @Reference
     private SlingRepository slingRepository;
 
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+    private Logger logger = LoggerFactory.getLogger(OnlineClipboardService.class);
+
+    protected void activate(final ComponentContext bundle) {
+        if (slingRepository != null) {
+            ResourceResolver resourceResolver = null;
+            Session session = null;
+            try {
+                session = slingRepository.loginAdministrative(null);
+                resourceResolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+                UserManager um = resourceResolver.adaptTo(UserManager.class);
+                Authorizable authorizable = um.getAuthorizable("anonymous");
+                Node root = session.getRootNode();
+                getOrCreatePath(session, session.getRootNode(), "content/fw");
+                if (session.hasPendingChanges()) {
+                    session.save();
+                }
+                AccessControlUtil.replaceAccessControlEntry(session, "/content/fw", authorizable.getPrincipal(),
+                        new String[]{}, new String[]{"jcr:read"},
+                        new String[]{}, null);
+                if (session.hasPendingChanges()) {
+                    session.save();
+                }
+            } catch (RepositoryException e) {
+                logger.error("Bundle Activation Error" + e.getMessage());
+            } catch (LoginException e) {
+                logger.error("Bundle Activation Error" + e.getMessage());
+            } finally {
+                if (resourceResolver != null) {
+                    resourceResolver.close();
+                }
+                if (session != null) {
+                    session.logout();
+                }
+            }
+        }
+    }
+
     private Node getOrCreatePath(Session session, Node node, String path) throws RepositoryException {
         String[] paths = path.split("/");
         Node currentNode = node;
-        for(int i = 0;i<paths.length;i++) {
+        for (int i = 0; i < paths.length; i++) {
             boolean exists = currentNode.hasNode(paths[i]);
-            if(!exists) {
-                currentNode.addNode(paths[i],JcrConstants.NT_UNSTRUCTURED);
+            if (!exists) {
+                currentNode.addNode(paths[i], JcrConstants.NT_UNSTRUCTURED);
             }
             currentNode = currentNode.getNode(paths[i]);
         }
@@ -44,37 +94,37 @@ public class OnlineClipboardServiceImpl implements OnlineClipboardService {
     }
 
     public void paste(ClipboardData data) throws Exception {
-       if(slingRepository != null) {
-           Session session = slingRepository.loginAdministrative(null);
-           try {
-               Node root = session.getRootNode();
-               Node content = root.getNode("content");
-               Node node = getOrCreatePath(session, content, "fw/oc/data");
-               String nodeName = System.currentTimeMillis() + "";
-               Node dataNode = node.addNode(nodeName, JcrConstants.NT_UNSTRUCTURED);
-               dataNode.setProperty("data",new String(data.getData(), "UTF-8"));
-               dataNode.setProperty("hostName",data.getHostName());
-               dataNode.setProperty("ipAddress",data.getIpAddress());
-               session.save();
-           } catch (RepositoryException e) {
-               throw e;
-           }  finally {
-               session.logout();
-           }
-       }
+        if (slingRepository != null) {
+            Session session = slingRepository.loginAdministrative(null);
+            try {
+                Node root = session.getRootNode();
+                Node content = root.getNode("content");
+                Node node = getOrCreatePath(session, content, "fw/oc/data");
+                String nodeName = System.currentTimeMillis() + "";
+                Node dataNode = node.addNode(nodeName, JcrConstants.NT_UNSTRUCTURED);
+                dataNode.setProperty("data", new String(data.getData(), "UTF-8"));
+                dataNode.setProperty("hostName", data.getHostName());
+                dataNode.setProperty("ipAddress", data.getIpAddress());
+                session.save();
+            } catch (RepositoryException e) {
+                throw e;
+            } finally {
+                session.logout();
+            }
+        }
     }
 
     @Override
     public JSONArray getData() throws Exception {
         JSONArray res = new JSONArray();
-        if(slingRepository != null) {
+        if (slingRepository != null) {
             Session session = slingRepository.loginAdministrative(null);
             try {
                 Node root = session.getRootNode();
                 Node content = root.getNode("content");
                 Node node = getOrCreatePath(session, content, "fw/oc/data");
                 NodeIterator iter = node.getNodes();
-                while(iter.hasNext()) {
+                while (iter.hasNext()) {
                     Node data = iter.nextNode();
                     JSONObject obj = new JSONObject();
                     obj.put("data", data.getProperty("data").getValue().toString());
